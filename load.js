bad1d2944c3fbb9a288cae07269a5e4244201cb2
Esotericnerd <vegasbrownmd@gmail.com>
2025-07-19 16:27:38 -0400

    Update load.js

--- load.js
+++ load.js
@@ -1,19 +1,15 @@
-import fs from 'fs';
-import path from 'path';
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    res.status(405).send('Method Not Allowed');
-    return;
-  }
+  if (req.method !== "GET") return res.status(405).send("Only GET allowed");
   const { filename } = req.query;
-  if (!filename) {
-    res.status(400).send('Missing filename');
-    return;
-  }
-  const dataDir = path.join(process.cwd(), 'data');
-  fs.readFile(path.join(dataDir, filename), 'utf8', (err, data) => {
-    if (err) return res.status(404).send('File not found');
-    res.json({ content: data });
-  });
+  if (!filename) return res.status(400).send("Missing filename");
+  await client.connect();
+  const db = client.db("emergencybriefcase");
+  const file = await db.collection("files").findOne({ filename });
+  if (!file) return res.status(404).send("File not found");
+  res.json({ content: file.content });
 }
